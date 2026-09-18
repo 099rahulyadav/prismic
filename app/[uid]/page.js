831c28c01c93bcac88733bb18980521e3ad8e864
Rahul Yadav <099rahul.yadav@gmail.com>
2026-09-19 04:05:22 +0530

    resolved deployment issue for dynamic pages for cloudflare pages.

--- app/[uid]/page.js
+++ app/[uid]/page.js
@@ -2,11 +2,24 @@ import { PrismicRichText } from "@prismicio/react";
 import { createClient } from "../../prismicio";
 import { notFound } from "next/navigation";
 
-export default async function DynamicPage({ params }) {
-  const { uid } = await params;
+export async function generateStaticParams() {
   const client = createClient();
 
-  const page = await client.getByUID("page", uid).catch(() => notFound());
+  const pages = await client.getAllByType("page");
+
+  return pages.map((page) => ({
+    uid: page.uid,
+  }));
+}
+
+export default async function DynamicPage({ params }) {
+  const { uid } = await params;
+
+  const client = createClient();
+
+  const page = await client
+    .getByUID("page", uid)
+    .catch(() => notFound());
 
   return (
     <main className="prose">
